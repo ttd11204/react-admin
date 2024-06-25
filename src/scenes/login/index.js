1c177e4f76c3cd33dba02cd2829dd594be3ee096
ttd11204 <datkk112004@gmail.com>
2024-06-25 22:05:28 +0700

    fix login

--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -2,63 +2,31 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./loginTest.css";
 import { loginApi } from "../../api/usersApi";
-import { registerStaffApi } from "../../api/registerApi";
 import ClipLoader from "react-spinners/ClipLoader";
-import {
-  validateFullName,
-  validateEmail,
-  validatePassword,
-  validateConfirmPassword,
-} from "../formValidation";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
 
 const Login = () => {
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
-  const [isLogin, setIsLogin] = useState(true);
   const [message, setMessage] = useState("");
   const [messageType, setMessageType] = useState("");
   const [loading, setLoading] = useState(false);
-
-  const [fullNameValidation, setFullNameValidation] = useState({
-    isValid: true,
-    message: "",
-  });
-  const [emailValidation, setEmailValidation] = useState({
-    isValid: true,
-    message: "",
-  });
-  const [passwordValidation, setPasswordValidation] = useState({
-    isValid: true,
-    message: "",
-  });
-  const [confirmPasswordValidation, setConfirmPasswordValidation] = useState({
-    isValid: true,
-    message: "",
-  });
 
   const navigate = useNavigate();
 
   useEffect(() => {
     const container = document.getElementById("container");
-    const registerBtn = document.getElementById("register");
     const loginBtn = document.getElementById("login");
 
-    const addActiveClass = () => container.classList.add("active");
     const removeActiveClass = () => container.classList.remove("active");
 
-    if (registerBtn && loginBtn) {
-      registerBtn.addEventListener("click", addActiveClass);
+    if (loginBtn) {
       loginBtn.addEventListener("click", removeActiveClass);
     }
 
     return () => {
-      if (registerBtn && loginBtn) {
-        registerBtn.removeEventListener("click", addActiveClass);
+      if (loginBtn) {
         loginBtn.removeEventListener("click", removeActiveClass);
       }
     };
@@ -115,183 +83,37 @@ const Login = () => {
     }
   };
 
-  const handleRegister = async (e) => {
-    e.preventDefault();
-
-    const fullNameValidation = validateFullName(fullName);
-    const emailValidation = await validateEmail(email);
-    const passwordValidation = validatePassword(password);
-    const confirmPasswordValidation = validateConfirmPassword(
-      password,
-      confirmPassword
-    );
-
-    setFullNameValidation(fullNameValidation);
-    setEmailValidation(emailValidation);
-    setPasswordValidation(passwordValidation);
-    setConfirmPasswordValidation(confirmPasswordValidation);
-
-    if (
-      !fullNameValidation.isValid ||
-      !emailValidation.isValid ||
-      !passwordValidation.isValid ||
-      !confirmPasswordValidation.isValid
-    ) {
-      setMessage("Please try again");
-      setMessageType("error");
-      return;
-    }
-
-    setLoading(true);
-
-    try {
-      const response = await registerStaffApi(
-        fullName,
-        email,
-        password,
-        confirmPassword
-      );
-      toast.success("Registration successful!");
-      setMessage("SIGN UP SUCCESSFULLY - LOG IN NOW !");
-      setMessageType("success");
-      setIsLogin(true);
-    } catch (error) {
-      if (error.response) {
-        toast.error(error.response.data.message || "Registration failed");
-      } else if (error.request) {
-        toast.error("No response from server");
-      } else {
-        toast.error(error.message);
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <div className="login-component">
-      <div className={`container ${!isLogin ? "active" : ""}`} id="container">
-        {isLogin ? (
-          <div className="form-container sign-in">
-            <form onSubmit={handleLogin}>
-              <h1 className="login_register">LOG IN</h1>
-              <span>or use your account for login</span>
-              <input
-                type="text"
-                value={email}
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <input
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <a href="#">Forgot Password</a>
-              <button type="submit" className="signInBtn">
-                {loading && <i className="fas fa-sync fa-spin"></i>}
-                Sign In
-              </button>
-              {message && (
-                <p className={messageType === "error" ? "error-message" : ""}>
-                  {message}
-                </p>
-              )}
-            </form>
-          </div>
-        ) : (
-          <div className="form-container sign-up">
-            <form onSubmit={handleRegister}>
-              <h1 className="login_register">Create Account</h1>
-              <input
-                type="text"
-                value={fullName}
-                placeholder="FullName"
-                onChange={(e) => setFullName(e.target.value)}
-                required
-                className={fullNameValidation.isValid ? "" : "error-input"}
-              />
-              {fullNameValidation.message && (
-                <p className="errorVal">{fullNameValidation.message}</p>
-              )}
-              <input
-                type="text"
-                value={email}
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                className={emailValidation.isValid ? "" : "error-input"}
-              />
-              {emailValidation.message && (
-                <p className="errorVal">{emailValidation.message}</p>
-              )}
-              <input
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                className={passwordValidation.isValid ? "" : "error-input"}
-              />
-              {passwordValidation.message && (
-                <p className="errorVal">{passwordValidation.message}</p>
-              )}
-              <input
-                type="password"
-                value={confirmPassword}
-                placeholder="Confirm Password"
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-                className={
-                  confirmPasswordValidation.isValid ? "" : "error-input"
-                }
-              />
-              {confirmPasswordValidation.message && (
-                <p className="errorVal">{confirmPasswordValidation.message}</p>
-              )}
-              <button type="submit" className="signUpBtn" disabled={loading}>
-                {loading ? <ClipLoader size={15} color="#fff" /> : "Sign Up"}
-              </button>
-              {message && (
-                <p className={messageType === "error" ? "error-message" : ""}>
-                  {message}
-                </p>
-              )}
-            </form>
-          </div>
-        )}
-        <div className="toggle-container">
-          <div className="toggle">
-            <div className="toggle-panel toggle-left">
-              <h1>badminton is joy</h1>
-              <p>Enter your userFullName password to schedule now!!</p>
-              <button
-                className="hidden"
-                id="login"
-                onClick={() => setIsLogin(true)}
-              >
-                Sign In
-              </button>
-            </div>
-            <div className="toggle-panel toggle-right">
-              <h1>badminton is life</h1>
-              <p>
-                Register with your personal details to use all of the site
-                features!!
-              </p>
-              <button
-                className="hidden"
-                id="register"
-                onClick={() => setIsLogin(false)}
-              >
-                Sign Up
-              </button>
-            </div>
-          </div>
-        </div>
+      <div className="container" id="container">
+        <form onSubmit={handleLogin}>
+          <h1 className="login_register">LOG IN</h1>
+          <span>or use your account for login</span>
+          <input
+            type="text"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <a href="#">Forgot Password</a>
+          <button type="submit" className="signInBtn">
+            {loading && <ClipLoader size={15} color="#fff" />}
+            {!loading && "Sign In"}
+          </button>
+          {message && (
+            <p className={messageType === "error" ? "error-message" : ""}>
+              {message}
+            </p>
+          )}
+        </form>
       </div>
     </div>
   );
