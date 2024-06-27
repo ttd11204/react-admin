df40fffe303c42dd9718853c3bbf30ac5533147c
ttd11204 <datkk112004@gmail.com>
2024-06-27 22:49:35 +0700

    add email

--- src/staff/calendar/fixed/Fix.jsx
+++ src/staff/calendar/fixed/Fix.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import { Box, Typography, Button, TextField, FormControl, FormControlLabel, Checkbox, Grid, Paper, ThemeProvider, createTheme } from "@mui/material";
 import CalendarView from './CalendarView';
 import { fetchPrice } from '../../../api/priceApi';
+import { fetchUserDetailByEmail, fetchUserDetail } from "../../../api/userApi"; // Import thêm các hàm API để kiểm tra email
 
 const theme = createTheme({
   palette: {
@@ -70,6 +71,10 @@ const FixedBooking = () => {
   const [slotEndTime, setSlotEndTime] = useState('');
   const [weekdayPrice, setWeekdayPrice] = useState(0);
   const [weekendPrice, setWeekendPrice] = useState(0);
+  const [email, setEmail] = useState('');
+  const [userExists, setUserExists] = useState(false);
+  const [userInfo, setUserInfo] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -95,22 +100,62 @@ const FixedBooking = () => {
     );
   };
 
+  const handleCheck = async () => {
+  if (!email) {
+    setErrorMessage('Vui lòng nhập email.');
+    return;
+  }
+  try {
+    const userData = await fetchUserDetailByEmail(email);
+    if (userData && userData.length > 0) {
+      const user = userData[0];
+      const detailedUserInfo = await fetchUserDetail(user.id);
+      if (detailedUserInfo) {
+        setUserExists(true);
+        setUserId(user.id); // Cập nhật userId
+        setUserInfo({
+          userId: user.id,
+          userName: user.userName,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          fullName: detailedUserInfo.fullName,
+          balance: detailedUserInfo.balance,
+          address: detailedUserInfo.address,
+        });
+        setErrorMessage('');
+      } else {
+        setUserExists(false);
+        setUserInfo(null);
+        setErrorMessage('Không tìm thấy chi tiết người dùng.');
+      }
+    } else {
+      setUserExists(false);
+      setUserInfo(null);
+      setErrorMessage('Người dùng không tồn tại. Vui lòng đăng ký.');
+    }
+  } catch (error) {
+    console.error('Lỗi khi kiểm tra tồn tại của người dùng:', error);
+    setErrorMessage('Lỗi khi kiểm tra tồn tại của người dùng. Vui lòng thử lại.');
+  }
+};
+
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const formattedStartDate = startDate.toISOString().split('T')[0];
-
+  
     const isWeekday = (day) => {
       const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
       return weekdays.includes(day);
     };
-
+  
     const totalDays = getTotalDaysForWeekdays(daysOfWeek, numberOfMonths, startDate);
-
+  
     const totalPrice = daysOfWeek.reduce((total, day) => {
       const pricePerDay = isWeekday(day) ? weekdayPrice : weekendPrice;
       return total + (totalDays[day] * pricePerDay);
     }, 0);
-
+  
     const bookingRequests = daysOfWeek.map(day => ({
       slotDate: formattedStartDate,
       timeSlot: {
@@ -119,23 +164,30 @@ const FixedBooking = () => {
       },
       price: isWeekday(day) ? weekdayPrice : weekendPrice,
     }));
-
-    console.log('Total Price:', totalPrice);
+  
+    const state = {
+      branchId,
+      bookingRequests,
+      totalPrice,
+      userId, // Truyền cả userId
+      email,  // và email
+      numberOfMonths,
+      daysOfWeek,
+      startDate: formattedStartDate,
+      type: 'fix',
+      slotStartTime,
+      slotEndTime,
+    };
+  
+    console.log('state:', state);
 
     navigate("/fixed-payment", {
-      state: {
-        branchId,
-        bookingRequests,
-        totalPrice,
-        userId,
-        numberOfMonths,
-        daysOfWeek,
-        startDate: formattedStartDate,
-        slotStartTime,
-        slotEndTime,
-      },
+      state
     });
   };
+
+  
+  
 
   return (
     <ThemeProvider theme={theme}>
@@ -149,103 +201,126 @@ const FixedBooking = () => {
               <Typography variant="h4" mb={2} sx={{ textAlign: 'center', color: 'primary.main', fontWeight: 'bold' }}>Fixed Booking</Typography>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <TextField
-                      label="Number of Months"
-                      type="number"
-                      value={numberOfMonths}
-                      onChange={(e) => setNumberOfMonths(e.target.value)}
-                      required
-                      InputLabelProps={{ style: { color: 'black' } }}
-                      InputProps={{ style: { color: 'black' } }}
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography sx={{ color: 'black' }}>Day of Week:</Typography>
-                  {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => (
-                    <FormControlLabel
-                      key={day}
-                      control={
-                        <Checkbox
-                          value={day}
-                          onChange={handleDayOfWeekChange}
-                          checked={daysOfWeek.includes(day)}
-                          sx={{ color: 'primary.main' }}
-                        />
-                      }
-                      label={day}
-                      sx={{ color: 'black' }}
-                    />
-                  ))}
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography sx={{ color: 'black' }}>Start Date:</Typography>
-                  <DatePicker
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                    dateFormat="yyyy-MM-dd"
-                    required
-                    popperPlacement="right-start"
-                    minDate={new Date()}
-                    customInput={<TextField sx={{ width: '100%' }} />}
+                  <TextField
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    sx={{ marginBottom: '10px', marginRight: '10px' }}
                   />
+                  <Button variant="contained" color="primary" onClick={handleCheck}>
+                    Check
+                  </Button>
+                  {errorMessage && (
+                    <Typography variant="body2" color="error">
+                      {errorMessage}
+                    </Typography>
+                  )}
                 </Grid>
-                <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <TextField
-                      label="User ID"
-                      type="text"
-                      value={userId}
-                      onChange={(e) => setUserId(e.target.value)}
-                      required
-                      InputLabelProps={{ style: { color: 'black' } }}
-                      InputProps={{ style: { color: 'black' } }}
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <TextField
-                      label="Branch ID"
-                      type="text"
-                      value={branchId}
-                      onChange={(e) => setBranchId(e.target.value)}
-                      required
-                      InputLabelProps={{ style: { color: 'black' } }}
-                      InputProps={{ style: { color: 'black' } }}
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <TextField
-                      label="Slot Start Time"
-                      type="text"
-                      value={slotStartTime}
-                      onChange={(e) => setSlotStartTime(e.target.value)}
-                      required
-                      InputLabelProps={{ style: { color: 'black' } }}
-                      InputProps={{ style: { color: 'black' } }}
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl fullWidth>
-                    <TextField
-                      label="Slot End Time"
-                      type="text"
-                      value={slotEndTime}
-                      onChange={(e) => setSlotEndTime(e.target.value)}
-                      required
-                      InputLabelProps={{ style: { color: 'black' } }}
-                      InputProps={{ style: { color: 'black' } }}
-                    />
-                  </FormControl>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button variant="contained" color="primary" type="submit" fullWidth>Continue</Button>
-                </Grid>
+                {userExists && userInfo && (
+                  <>
+                    <Grid item xs={12}>
+                      <Typography variant="h6" color="black">
+                        <strong>Username:</strong> {userInfo.userName ? userInfo.userName : 'N/A'}
+                      </Typography>
+                      <Typography variant="h6" color="black">
+                        <strong>Full Name:</strong> {userInfo.fullName ? userInfo.fullName : 'N/A'}
+                      </Typography>
+                      <Typography variant="h6" color="black">
+                        <strong>Phone:</strong> {userInfo.phoneNumber ? userInfo.phoneNumber : 'N/A'}
+                      </Typography>
+                      <Typography variant="h6" color="black">
+                        <strong>Coin:</strong> {userInfo.balance ? userInfo.balance : 'N/A'}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Number of Months"
+                          type="number"
+                          value={numberOfMonths}
+                          onChange={(e) => setNumberOfMonths(e.target.value)}
+                          required
+                          InputLabelProps={{ style: { color: 'black' } }}
+                          InputProps={{ style: { color: 'black' } }}
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography sx={{ color: 'black' }}>Day of Week:</Typography>
+                      {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => (
+                        <FormControlLabel
+                          key={day}
+                          control={
+                            <Checkbox
+                              value={day}
+                              onChange={handleDayOfWeekChange}
+                              checked={daysOfWeek.includes(day)}
+                              sx={{ color: 'primary.main' }}
+                            />
+                          }
+                          label={day}
+                          sx={{ color: 'black' }}
+                        />
+                      ))}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Typography sx={{ color: 'black' }}>Start Date:</Typography>
+                      <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        dateFormat="yyyy-MM-dd"
+                        required
+                        popperPlacement="right-start"
+                        minDate={new Date()}
+                        customInput={<TextField sx={{ width: '100%' }} />}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Branch ID"
+                          type="text"
+                          value={branchId}
+                          onChange={(e) => setBranchId(e.target.value)}
+                          required
+                          InputLabelProps={{ style: { color: 'black' } }}
+                          InputProps={{ style: { color: 'black' } }}
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Slot Start Time"
+                          type="text"
+                          value={slotStartTime}
+                          onChange={(e) => setSlotStartTime(e.target.value)}
+                          required
+                          InputLabelProps={{ style: { color: 'black' } }}
+                          InputProps={{ style: { color: 'black' } }}
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth>
+                        <TextField
+                          label="Slot End Time"
+                          type="text"
+                          value={slotEndTime}
+                          onChange={(e) => setSlotEndTime(e.target.value)}
+                          required
+                          InputLabelProps={{ style: { color: 'black' } }}
+                          InputProps={{ style: { color: 'black' } }}
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Button variant="contained" color="primary" type="submit" fullWidth>Continue</Button>
+                    </Grid>
+                  </>
+                )}
               </Grid>
             </Paper>
           </form>
@@ -253,6 +328,6 @@ const FixedBooking = () => {
       </Box>
     </ThemeProvider>
   );
-};
+};  
 
 export default FixedBooking;
